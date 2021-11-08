147f294ab84fe0bb6b014d9946c243b167f6ce1e
Brad <BradPatras@users.noreply.github.com>
2021-11-07 22:48:52 -0600

    implementing public api basics

--- basic-remote-configs/src/main/java/io/github/bradpatras/basicremoteconfigs/BasicRemoteConfigs.kt
+++ basic-remote-configs/src/main/java/io/github/bradpatras/basicremoteconfigs/BasicRemoteConfigs.kt
@@ -1,35 +1,84 @@
 package io.github.bradpatras.basicremoteconfigs
 
 import kotlinx.coroutines.coroutineScope
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.URL
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.collections.HashMap
+
+// Version code representing an unknown or un-fetched version
+private const val VERSION_NONE = -1
 
 class BasicRemoteConfigs(private val remoteUrl: URL) {
+    private var version: Int = VERSION_NONE
+    private var values: HashMap<String, Any> = HashMap()
+    private var expirationDate: Date? = null
 
-    suspend fun fetchConfigs(): String? = coroutineScope {
-        HttpRequestHelper(remoteUrl).makeGetRequest()
+    suspend fun fetchConfigs() = coroutineScope {
+        val configs = HttpRequestHelper(remoteUrl).makeGetRequest() ?: ""
+        val json = JSONObject(configs)
+        val newValues = HashMap<String, Any>()
+        json.keys().forEach { newValues[it] = json.get(it) }
+        values = newValues
+        expirationDate = getNewExpirationDate()
     }
 
-    fun getBoolean(key: String): Boolean? {
-        return true
+    private fun getNewExpirationDate(): Date {
+        val calendar = Calendar.getInstance()
+        calendar.time = Date()
+        calendar.add(Calendar.DATE, 1)
+        return calendar.time
     }
 
-    fun getInt(key: String): Int? {
-        return 0
+    private suspend fun getCurrentValues() = coroutineScope {
+        if (expirationDate?.after(Date()) != true || values.isEmpty()) {
+            fetchConfigs()
+        }
+
+        return@coroutineScope values
     }
 
-    fun getString(key: String): String? {
-        return "true"
+    suspend fun getBoolean(key: String): Boolean? {
+        return getCurrentValues()[key] as? Boolean?
     }
 
-    fun getBooleanArray(key: String): Array<Boolean>? {
-        return emptyArray()
+    suspend fun getInt(key: String): Int? {
+        return getCurrentValues()[key] as? Int
     }
 
-    fun getIntArray(key: String): Array<Int>? {
-        return emptyArray()
+    suspend fun getString(key: String): String? {
+        return getCurrentValues()[key] as? String
     }
 
-    fun getStringArray(key: String): Array<String>? {
-        return emptyArray()
+    suspend fun getBooleanArray(key: String): Array<Boolean>? {
+        val jsonArray = getCurrentValues()[key] as? JSONArray ?: return null
+        val values = mutableListOf<Boolean>()
+        for (i in 0 until jsonArray.length()) {
+            (jsonArray[i] as? Boolean)?.let { values.add(it) }
+        }
+
+        return values.toTypedArray()
+    }
+
+    suspend fun getIntArray(key: String): Array<Int>? {
+        val jsonArray = getCurrentValues()[key] as? JSONArray ?: return null
+        val values = mutableListOf<Int>()
+        for (i in 0 until jsonArray.length()) {
+            (jsonArray[i] as? Int)?.let { values.add(it) }
+        }
+
+        return values.toTypedArray()
+    }
+
+    suspend fun getStringArray(key: String): Array<String>? {
+        val jsonArray = getCurrentValues()[key] as? JSONArray ?: return null
+        val values = mutableListOf<String>()
+        for (i in 0 until jsonArray.length()) {
+            (jsonArray[i] as? String)?.let { values.add(it) }
+        }
+
+        return values.toTypedArray()
     }
 }
