e00eaab4ebfe1f31e1ada7746e33d3c47f3d1e3e
Brad <BradPatras@users.noreply.github.com>
2021-12-06 22:39:09 -0600

    updated demo app to have fetch button

--- app/src/main/java/io/github/bradpatras/brc_android_demo/MainActivity.kt
+++ app/src/main/java/io/github/bradpatras/brc_android_demo/MainActivity.kt
@@ -2,9 +2,10 @@ package io.github.bradpatras.brc_android_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import io.github.bradpatras.basicremoteconfigs.BasicRemoteConfigs
+import io.github.bradpatras.brc_android_demo.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 import java.net.URL
 
@@ -13,30 +14,39 @@ private const val CONFIG_URL = "https://github.com/BradPatras/basic-remote-confi
 class MainActivity : AppCompatActivity() {
 
     private val brc: BasicRemoteConfigs = BasicRemoteConfigs(URL(CONFIG_URL))
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        lifecycleScope.launchWhenCreated {
-            delay(1000)
-
-            try {
-                brc.fetchConfigs()
-                withContext(Dispatchers.Main) {
-                    updateText(brc.values.map { "${it.key}: ${it.value}" }
-                        .joinToString(separator = ",\n"))
-                }
-            } catch (error: Throwable) {
-                withContext(Dispatchers.Main) {
-                    updateText("Encountered an error when fetching configs")
-                }
+        binding.fetchBtn.setOnClickListener {
+            updateUI(null, true)
+            lifecycleScope.launch {
+                getConfigs()
             }
         }
     }
 
-    private fun updateText(newText: String?) {
-        val textView = findViewById<TextView>(R.id.center_tv)
-        textView.text = newText
+    private suspend fun getConfigs() = coroutineScope {
+        try {
+            brc.fetchConfigs()
+            withContext(Dispatchers.Main) {
+                updateUI(
+                    brc.values.map { "${it.key}: ${it.value}" }.joinToString(separator = ",\n"),
+                    false
+                )
+            }
+        } catch (error: Throwable) {
+            withContext(Dispatchers.Main) {
+                updateUI("Encountered an error when fetching configs", false)
+            }
+        }
+    }
+
+    private fun updateUI(newText: String?, isLoading: Boolean) {
+        binding.centerTv.text = newText
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.INVISIBLE
     }
 }
